4bf4399dcace38905c9fd4f32556312af87c07d6
DipeshWagh07 <dipesh@techsierra.in>
2026-03-17 00:32:31 +0530

    file renaming

--- lib/analytics/utils/metricsResolver.ts
+++ lib/analytics/utils/metricsResolver.ts
@@ -1,4 +1,4 @@
-import { METRIC_FORMULAS } from "./metricsDerived.ts"
+import { METRIC_FORMULAS } from "./metricsDerived.js"
 
 export const METRIC_DEPENDENCIES: Record<string, string[]> = {
   ts_meetings_duration_avg: [
