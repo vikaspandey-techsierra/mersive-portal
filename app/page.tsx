277bd12f61ec3bf446e6d23be01dd27d7638000a
DipeshWagh07 <dipesh@techsierra.in>
2026-03-31 17:13:02 +0530

    kept page as it is only changed routes through org

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,20 @@
 "use client";
-
 import { useRouter } from "next/navigation";
-
 export default function Home() {
   const router = useRouter();
 
   return (
     <main className="min-h-screen flex items-center justify-center px-6 bg-white text-black">
-      <div className="bg-white border rounded-2xl shadow-2xl p-10 max-w-lg w-full text-center flex flex-col gap-6">
+      <div className="bg-white/10 backdrop-blur-lg border border-white/20 rounded-2xl shadow-2xl p-10 max-w-lg w-full text-center flex flex-col gap-6">
         <h1 className="text-4xl font-bold mb-4 tracking-tight">
           📊 Analytics Dashboard
         </h1>
-
-        <div className="flex flex-col gap-4">
-          <button
-            onClick={() => router.push("/org_1/home")}
-            // onClick={() => router.push("/org_1/analytics")}
-            className="bg-violet-600 hover:bg-violet-700 px-6 py-3 rounded-lg text-white"
-          >
-            ORG 1
-          </button>
-
-          <button
-            onClick={() => router.push("/org_2/home")}
-            // onClick={() => router.push("/org_2/analytics")}
-            className="bg-gray-800 hover:bg-gray-900 px-6 py-3 rounded-lg text-white"
-          >
-            ORG 2
-          </button>
-
-          <button
-            onClick={() => router.push("/GDeZNiL4IS3QrYdLQTf6-clney/home")}
-            // onClick={() => router.push("/GDeZNiL4IS3QrYdLQTf6-clney/home")}
-            className="bg-green-600 hover:bg-green-700 px-6 py-3 rounded-lg text-white"
-          >
-            MOCK ORG
-          </button>
-        </div>
+        <button
+          onClick={() => router.push("/GDeZNiL4IS3QrYdLQTf6-clney/home")}
+          className="bg-violet-600 hover:bg-violet-700 transition-all duration-300 px-6 py-3 rounded-lg font-medium shadow-lg hover:scale-105 active:scale-95 text-white"
+        >
+          Analytics Dashboard →
+        </button>
       </div>
     </main>
   );
